ae8b013f6debd9da246698892d6744b1443d1466
IsaiasArce <isaiasarcerodriguez@gmail.com>
2022-08-17 23:08:46 -0600

    Change Spring Version

--- GraphQL/src/main/java/com/cenfotec/graphql/pjgraphql/PjGraphQlApplication.java
+++ GraphQL/src/main/java/com/cenfotec/graphql/pjgraphql/PjGraphQlApplication.java
@@ -1,4 +1,4 @@
-package com.cenfotec.graphql;
+package com.cenfotec.graphql.pjgraphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- GraphQL/src/test/java/com/cenfotec/graphql/pjgraphql/PjGraphQlApplicationTests.java
+++ GraphQL/src/test/java/com/cenfotec/graphql/pjgraphql/PjGraphQlApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cenfotec.graphql;
+package com.cenfotec.graphql.pjgraphql;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
